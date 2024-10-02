b22afcbbd4311ab14ef2f4118a60a698db1ccab1
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-10-02 16:55:12 +0530

    code cleaning _woosh_

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,11 @@
 export function encrypt(text, key) {
+  // Alphabets to get index and calculate new
   let alphabets = "abcdefghijklmnopqrstuvwxyz";
+
+  // Placeholder
   let encryptedString = "";
 
+  // Generating every single character of input
   for (let char of text) {
     // Checking for special char or uppercase
     if (!alphabets.includes(char)) {
@@ -13,17 +17,19 @@ export function encrypt(text, key) {
       }
     }
 
+    // Find the index of current
     const index = alphabets.indexOf(char);
-    let newIndex = index + key;
+    let newIndex = index + key; // Generate new index by adding key
 
-    if (newIndex > 25) newIndex %= 26;
+    if (newIndex > 25) newIndex %= 26; // Check for out of range
 
-    const newChar = alphabets[newIndex];
-    encryptedString += newChar;
+    const newChar = alphabets[newIndex]; // Find new character
+    encryptedString += newChar; // Append to encrypted string
 
-    alphabets = alphabets.toLowerCase();
+    alphabets = alphabets.toLowerCase(); // Transform to lowercase
   }
 
+  // Reverse the response to make encryption more robust
   return encryptedString.split("").reverse().join("");
 }
 
@@ -32,4 +38,15 @@ export function generateKey() {
   return Math.ceil(Math.random() * 100);
 }
 
-export function decrypt(text, key) {}
+export function decrypt(text, key) {
+  const reverseText = text.split("").reverse().join("");
+  const alphabets = "abcdefghijklmnopqrstuvwxyz";
+  const decryptedText = "";
+
+  for (let char of reverseText) {
+    const index = alphbaets[char];
+    const newIndex = index - key;
+    if (newIndex < 0) {
+    }
+  }
+}
